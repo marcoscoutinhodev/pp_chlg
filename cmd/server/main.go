cc1c6dcc93ff35bac226717e7ca80cc223b06fc0
marcoscoutinhodev@outlook.com <marcoscoutinhodev@outlook.com>
2023-08-27 16:28:41 -0300

    feat: add /user/signup endpoint

---
+++ cmd/server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+	"github.com/marcoscoutinhodev/pp_chlg/internal/infra/keycloak"
+	"github.com/marcoscoutinhodev/pp_chlg/internal/infra/repository"
+	"github.com/marcoscoutinhodev/pp_chlg/internal/infra/validator"
+	"github.com/marcoscoutinhodev/pp_chlg/internal/presentation/controller"
+	"github.com/marcoscoutinhodev/pp_chlg/internal/usecase"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+}
+
+func main() {
+	mux := chi.NewRouter()
+	mux.Use(middleware.Logger)
+
+	identityManager := keycloak.NewIdentityManager()
+
+	mongoClient := repository.NewMongoClient(context.Background())
+	userRepository := repository.NewUserRepository(mongoClient)
+
+	userValidator := validator.NewUserValidator()
+
+	userAuthenticationUseCase := usecase.NewUserAuthentication(identityManager, userValidator, userRepository)
+	userAuthenticationController := controller.NewUserAuthenticationController(*userAuthenticationUseCase)
+
+	mux.Route("/user", func(r chi.Router) {
+		r.Post("/signup", userAuthenticationController.CreateUser)
+	})
+
+	http.ListenAndServe(os.Getenv("SERVER_PORT"), mux)
+}
