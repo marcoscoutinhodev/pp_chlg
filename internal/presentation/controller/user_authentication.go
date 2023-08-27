944f4e322a3079fb1433a8c7886d7492eb538b4e
marcoscoutinhodev@outlook.com <marcoscoutinhodev@outlook.com>
2023-08-27 16:08:16 -0300

    feat: add UserAuthentication.CreateUser controller

---
+++ internal/presentation/controller/user_authentication.go
@@ -0,0 +1,50 @@
+package controller
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/marcoscoutinhodev/pp_chlg/internal/usecase"
+)
+
+type UserAuthenticationController struct {
+	UserAuthenticationUseCase usecase.UserAuthentication
+}
+
+func NewUserAuthenticationController(uauc usecase.UserAuthentication) *UserAuthenticationController {
+	return &UserAuthenticationController{
+		UserAuthenticationUseCase: uauc,
+	}
+}
+
+func (u UserAuthenticationController) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var input usecase.UserAuthentication_CreateUserInputDTO
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"success": false,
+			"error":   "failed to parse request body",
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	output, err := u.UserAuthenticationUseCase.CreateUser(ctx, &input)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"success": false,
+			"error":   "internal error, try again in a few minutes",
+		})
+		fmt.Printf("internal error: %v\n", err)
+		return
+	}
+
+	w.WriteHeader(output.StatusCode)
+	json.NewEncoder(w).Encode(output)
+}
