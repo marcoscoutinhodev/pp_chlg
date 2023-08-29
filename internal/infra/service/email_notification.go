8329832e13a0e5cfb2f7125d361b4ba60f896e9c
marcoscoutinhodev@outlook.com <marcoscoutinhodev@outlook.com>
2023-08-29 14:14:21 -0300

    refactor: change EmailNotification to send data to a queue

--- internal/infra/service/email_notification.go
+++ internal/infra/service/email_notification.go
@@ -1,18 +1,73 @@
 package service
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/marcoscoutinhodev/pp_chlg/internal/entity"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-type EmailNotificationService struct{}
-
-func NewEmailNotificationService() *EmailNotificationService {
-	return &EmailNotificationService{}
+type EmailNotificationService struct {
+	conn *amqp.Connection
 }
 
-func (e EmailNotificationService) TransferNotification(payer, payee *entity.User, amount float64) {
-	fmt.Printf("Enviando email para cliente: %s | %s. Total transferido: %.2f\n", payer.ID, payer.Email, amount)
-	fmt.Printf("Enviando email para cliente: %s | %s. Total Recebido: %.2f\n", payee.ID, payee.Email, amount)
+func NewEmailNotificationService() *EmailNotificationService {
+	c, err := amqp.Dial(os.Getenv("RBMQ_URI"))
+	if err != nil {
+		panic(err)
+	}
+
+	return &EmailNotificationService{
+		conn: c,
+	}
+}
+
+type transferNotificationdDTO struct {
+	PayerEmail        string  `json:"payer_email"`
+	PayerName         string  `json:"payer_name"`
+	PayeeEmail        string  `json:"payee_email"`
+	PayeeName         string  `json:"payee_name"`
+	AmountTransferred float64 `json:"amount_transferred"`
+}
+
+func (e EmailNotificationService) TransferNotification(ctx context.Context, payer, payee entity.User, amount float64) error {
+	ch, err := e.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	defer ch.Close()
+
+	input := transferNotificationdDTO{
+		PayerEmail:        payer.Email,
+		PayerName:         fmt.Sprintf("%s %s", payer.FirstName, payer.LastName),
+		PayeeEmail:        payee.Email,
+		PayeeName:         fmt.Sprintf("%s %s", payee.FirstName, payee.LastName),
+		AmountTransferred: amount,
+	}
+	body, err := json.Marshal(&input)
+	if err != nil {
+		return err
+	}
+
+	err = ch.PublishWithContext(
+		ctx,
+		os.Getenv("RBMQ_TRANSFER_NOTIFICATION_EXCHANGE"),
+		os.Getenv("RBMQ_TRANSFER_NOTIFICATION_KEY"),
+		true,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        body,
+		},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
